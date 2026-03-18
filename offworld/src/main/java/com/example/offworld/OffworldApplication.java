e4472b87f4835af3e9372d79c30501dd65eb8c0f
Matthias Radin <matthiasradin7@gmail.com>
2026-03-18 17:24:53 +0100

    initialisation

---
+++ offworld/src/main/java/com/example/offworld/OffworldApplication.java
@@ -0,0 +1,13 @@
+package com.example.offworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OffworldApplication.class, args);
+	}
+
+}
---
+++ offworld/src/test/java/com/example/offworld/OffworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.offworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
